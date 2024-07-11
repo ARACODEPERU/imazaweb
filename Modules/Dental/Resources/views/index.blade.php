fb39524910bd7630f9f410e441124e8da8d9b4fe
Elmer Rodriguez <elrodriguez2423@gmail.com>
2024-07-11 09:15:16 -0500

    limpio

--- Modules/Dental/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('dental::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('dental.name') !!}
-    </p>
-@endsection
--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.imaza')
-@section('title', 'Inicio')
-@section('content')
-    <div class="container">
-
-    </div>
-@stop
