0fc7f26765762cbbf504c6f71b0b01b287a3c224
Yisus Web <connexion.jesus@gmail.com>
2024-07-21 03:07:34 -0500

    caracteristicas capacitacion - home

--- app/View/Components/Features.php
+++ app/View/Components/Features.php
@@ -5,15 +5,25 @@ namespace App\View\Components;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
+use Modules\CMS\Entities\CmsSection;
 
 class Features extends Component
 {
-    /**
-     * Create a new component instance.
-     */
+    protected $features;
+
     public function __construct()
     {
-        //
+
+        $this->features = CmsSection::where('component_id', 'caracteristicas_capacitacion_area_3')
+            ->join('cms_section_items', 'section_id', 'cms_sections.id')
+            ->join('cms_items', 'cms_section_items.item_id', 'cms_items.id')
+            ->select(
+                'cms_items.content',
+                'cms_section_items.position'
+            )
+            ->orderBy('cms_section_items.position')
+            ->get();
+
     }
 
     /**
@@ -21,6 +31,8 @@ class Features extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.features');
+        return view('components.features', [
+            'features' => $this->features
+        ]);
     }
 }
