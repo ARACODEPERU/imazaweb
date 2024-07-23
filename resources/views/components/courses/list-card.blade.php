3963e6c9cb2ad32f8851c942dbe4093256f14068
Yisus Web <connexion.jesus@gmail.com>
2024-07-23 03:50:54 -0500

    Titulo de la seccion cursos en home

--- resources/views/components/courses/list-card.blade.php
+++ resources/views/components/courses/list-card.blade.php
@@ -1,8 +1,8 @@
 <section class="courses-one">
     <div class="container">
         <div class="section-title text-center">
-            <span class="section-title__tagline">Checkout New List</span>
-            <h2 class="section-title__title">Explore Courses</h2>
+            <span class="section-title__tagline">Nuevos ingresos en</span>
+            <h2 class="section-title__title"> Cursos - Talleres</h2>
         </div>
         <div class="row">
             <!--Start Single Courses One-->
