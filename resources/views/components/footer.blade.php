241b2e7672ba2a26056dd5a7397c82170a16f779
Yisus Web <connexion.jesus@gmail.com>
2024-08-09 16:24:06 -0500

    imagen footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer-one">
     <div class="footer-one__bg"
-        style="background-image: url({{ URL('themes/imazaweb/images/backgrounds/footer-v1-bg.jpg') }});">
+        style="background-image: url({{ URL('themes/imazaweb/images/footer.jpg') }});">
     </div><!-- /.footer-one__bg -->
     <div class="footer-one__top">
         <div class="container">
@@ -9,9 +9,10 @@
                 <div class="col-xl-2 col-lg-4 col-md-4 wow animated fadeInUp" data-wow-delay="0.1s">
                     <div class="footer-widget__column footer-widget__about">
                         <div class="footer-widget__about-logo">
-                            <a href="index.html"><img
-                                    src="{{ URL('themes/imazaweb/images/resources/footer-logo.png') }}"
-                                    alt=""></a>
+                            <a href="{{ route('index_main') }}">
+                                <img style="height: 120px;" src="{{ URL('themes/imazaweb/images/isotipoNegativo.png') }}"
+                                    alt="">
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -20,7 +21,7 @@
                 <!--Start Footer Widget Column-->
                 <div class="col-xl-2 col-lg-4 col-md-4 wow animated fadeInUp" data-wow-delay="0.3s">
                     <div class="footer-widget__column footer-widget__courses">
-                        <h3 class="footer-widget__title">Courses</h3>
+                        <h3 class="footer-widget__title">Cursos</h3>
                         <ul class="footer-widget__courses-list list-unstyled">
                             <li><a href="#">UI/UX Design</a></li>
                             <li><a href="#">WordPress Development</a></li>
@@ -50,7 +51,7 @@
                 <!--Start Footer Widget Column-->
                 <div class="col-xl-3 col-lg-6 col-md-6 wow animated fadeInUp" data-wow-delay="0.7s">
                     <div class="footer-widget__column footer-widget__contact">
-                        <h3 class="footer-widget__title">Contact</h3>
+                        <h3 class="footer-widget__title">Contactanos</h3>
                         <p class="text">88 broklyn street, New York USA</p>
                         <p><a href="mailto:info@templatepath.com">needhelp@company.com</a></p>
                         <p class="phone"><a href="tel:123456789">92 888 666 0000</a></p>
@@ -62,10 +63,11 @@
                 <div class="col-xl-3 col-lg-6 col-md-6 wow animated fadeInUp" data-wow-delay="0.9s">
                     <div class="footer-widget__column footer-widget__social-links">
                         <ul class="footer-widget__social-links-list list-unstyled clearfix">
-                            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fab fa-facebook"></i></a></li>
+                            <li><a href="#" target="_blank"><i class="fab fa-tiktok"></i></a></li>
+                            {{-- <li><a href="#"><i class="fab fa-twitter"></i></a></li>
                             <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                            <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                            <li><a href="#"><i class="fab fa-dribbble"></i></a></li> --}}
                         </ul>
                     </div>
                 </div>
