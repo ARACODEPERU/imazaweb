3709b687e95a3c7334891a05f2dbef717fc4d60e
Yisus Web <connexion.jesus@gmail.com>
2024-10-15 11:46:59 -0500

    dr

--- app/View/Components/Header.php
+++ app/View/Components/Header.php
@@ -14,7 +14,6 @@ class Header extends Component
 
     public function __construct()
     {
-
         $this->header = CmsSection::where('component_id', 'header_area_1')
             ->join('cms_section_items', 'section_id', 'cms_sections.id')
             ->join('cms_items', 'cms_section_items.item_id', 'cms_items.id')
